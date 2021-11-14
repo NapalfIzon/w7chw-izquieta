19ef912fa386273e0118fb2baf3ad40574e8b9d5
Napoleon Izquieta <napoleon.izquieta@gmail.com>
2021-11-14 22:45:21 +0100

    :arrow_up: Adding userThunk

---
+++ src/redux/thunk/userThunk.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import jwtDecode from "jwt-decode";
+import { registerUserAction } from "../actions/actionCreators";
+
+const registerUserThunk = (user) => async (dispatch) => {
+  const response = await axios.post(
+    process.env.REACT_APP_API_ENDPOINT + "/users/register",
+    user
+  );
+
+  if (response.status === 201) {
+    const token = response.data.token;
+    const user = jwtDecode(token);
+    dispatch(registerUserAction(user));
+  } // TODO preguntar por status diferentes al 201 (ya que está designado así al crear al usuario)
+};
+
+export default registerUserThunk;
